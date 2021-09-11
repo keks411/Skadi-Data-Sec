a71ae7d8f0e01f971186d4a060bea05c04ef0158
user <user@DESKTOP-SS4QEBQ>
2021-09-10 19:01:50 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace FLOR
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnDown = new System.Windows.Forms.Button();
+            this.tBoxConsole = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnDown
+            // 
+            this.BtnDown.Location = new System.Drawing.Point(38, 31);
+            this.BtnDown.Name = "BtnDown";
+            this.BtnDown.Size = new System.Drawing.Size(75, 23);
+            this.BtnDown.TabIndex = 0;
+            this.BtnDown.Text = "Scan";
+            this.BtnDown.UseVisualStyleBackColor = true;
+            this.BtnDown.Click += new System.EventHandler(this.BtnDown_Click);
+            // 
+            // tBoxConsole
+            // 
+            this.tBoxConsole.Location = new System.Drawing.Point(12, 127);
+            this.tBoxConsole.Multiline = true;
+            this.tBoxConsole.Name = "tBoxConsole";
+            this.tBoxConsole.ReadOnly = true;
+            this.tBoxConsole.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tBoxConsole.Size = new System.Drawing.Size(650, 161);
+            this.tBoxConsole.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 278);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(674, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 300);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tBoxConsole);
+            this.Controls.Add(this.BtnDown);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FLOR - Data-Sec GmbH";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnDown;
+        private System.Windows.Forms.TextBox tBoxConsole;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Principal;
+using Ionic.Zip;
+using System.Diagnostics;
+using System.Threading;
+
+
+
+namespace FLOR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            //check for admin or not, do not allow non-admin and close
+            bool IsAdm = Convert.ToBoolean(IsAdministrator());
+            if (IsAdm == false)
+            {
+                MessageBox.Show("Application must be run as ADMIN!", "Missing Privs");
+                System.Windows.Forms.Application.Exit();
+            }
+
+            //color statusstrip
+            toolStripStatusLabel1.Text = "ELEVATED";
+            toolStripStatusLabel1.ForeColor = Color.Green;
+
+            //clear console window
+            tBoxConsole.Text = "";
+
+            //write basic info into window
+            tBoxConsole.Text = "hallo";
+            tBoxConsole.Text += System.Environment.NewLine + "aaa";
+            
+            
+        }
+
+        public static bool IsAdministrator()
+        {
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+        private void BtnDown_Click(object sender, EventArgs e)
+        {
+            //downloading scanner zip
+            string DownPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string DownFile = DownPath + "\\ds.zip";
+            MessageBox.Show(DownPath);
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("https://dstoolsiocsearch.blob.core.windows.net/ioc1tools/ds.zip", DownFile);
+
+            //extract zip
+            using (Ionic.Zip.ZipFile zip = Ionic.Zip.ZipFile.Read(DownFile))
+            {
+                zip.Password = "1234";
+                zip.ExtractAll(DownPath, Ionic.Zip.ExtractExistingFileAction.DoNotOverwrite);
+            }
+
+            //start upgrader
+            MessageBox.Show("Starting Online-Update");
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "loki-upgrader.exe";
+            
+        }
+
+
+
+
+    }
+}
